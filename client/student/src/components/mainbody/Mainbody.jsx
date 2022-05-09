088504fc6c99747c3b4ccaed4e491b13b0473ba5
Spritan <proyashpsb@gmail.com>
2022-05-09 19:05:20 +0530

    student attendence done

--- client/student/src/components/mainbody/Mainbody.jsx
+++ client/student/src/components/mainbody/Mainbody.jsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import AttendenceCards from '../attendenceCards/AttendenceCards';
 
 import "./mainbody.scss";
 
@@ -6,79 +7,25 @@ const Mainbody = () => {
 
   return (
     <>
-      <div className="main">
-        <div className='flex1'></div>
-        <div className="mainbody">
-          <h1>Attendance Report</h1>
-          <div className='formsSection'>
-            <div>Date: &ensp;
-              <input type="date" name="" id="" /></div>
-            <div><label for="Course">Course:</label>
-              &ensp;
-              <select name="Course" id="Course">
-                <option value="B.Tech">B.Tech</option>
-                <option value="B.Ca">B.Ca</option>
-                <option value="M.Ca">M.Ca</option>
-                {/* <option value="audi">Audi</option> */}
-              </select></div>
-            <div>
-              <label for="Semester">Semester:</label>
-              &ensp;
-              <select name="Semester" id="Semester">
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                {/* <option value="audi">Audi</option> */}
-              </select>
-            </div>
-
+      <div className="container">
+        <div className='flex'></div>
+        <div className="container2">
+          <div className="flexcol">
+            <AttendenceCards />
+            <AttendenceCards />
+            <AttendenceCards />
           </div>
-          <br />
-          <div className='formTable'>
-            <table>
-              <tr>
-                <th>Name</th>
-                <th>Roll No.</th>
-                <th>Attendence</th>
-              </tr>
-              <tr>
-                <td>Alfreds Futterkiste</td>
-                <td>Maria Anders</td>
-                <td><input type="checkbox" id='switch' /></td>
-              </tr>
-              <tr>
-                <td>Centro comercial Moctezuma</td>
-                <td>Francisco Chang</td>
-                <td><input type="checkbox" id='switch' /></td>
-              </tr>
-              <tr>
-                <td>Ernst Handel</td>
-                <td>Roland Mendel</td>
-                <td><input type="checkbox" id='switch' /></td>
-              </tr>
-              <tr>
-                <td>Island Trading</td>
-                <td>Helen Bennett</td>
-                <td><input type="checkbox" id='switch' /></td>
-              </tr>
-              <tr>
-                <td>Laughing Bacchus Winecellars</td>
-                <td>Yoshi Tannamuri</td>
-                <td><input type="checkbox" id='switch' /></td>
-              </tr>
-              <tr>
-                <td>Magazzini Alimentari Riuniti</td>
-                <td>Giovanni Rovelli</td>
-                <td><input type="checkbox" id='switch' /></td>
-              </tr>
-            </table>
-
-              <input type="checkbox" id='switch' />
-
+          <div className="flexcol">
+            <AttendenceCards />
+            <AttendenceCards />
+            <AttendenceCards />
           </div>
+          
         </div>
-        <div className='flex3'></div>
+        <div className='flex'></div>
+
       </div>
+      }
     </>
   )
 }
--- client/student/src/pages/profile/Profile.jsx
+++ client/student/src/pages/profile/Profile.jsx
@@ -26,13 +26,16 @@ const Profile = () => {
                     </div>
                     <div className="profileBody2">
                         <div className="profileCol3">
-                            <p className="Tags">Name: <span className="User Name" >Proyash Paban Sarma Borah</span></p>
-                            <p className="Tags">Roll No.: <span className="User rollNo" >123</span></p>
-                            <p className="Tags">Course: <span className="User course" >123</span></p>
-                            <p className="Tags">Depatment: <span className="User Department" >Electronics and Telecommunication Engineering</span></p>
+                            <p className="Tags">Email: <span className="User Name" >proyash20-331@aec.ac.in</span></p>
+                            <p className="Tags">address: <span className="User rollNo" >Panikara Gaon, Morangi Chari Ali, dist: Golaghat Golaghat, Assam, 785702 India</span></p>
+                            <p className="Tags">phoneNumber: <span className="User course" >6913911634</span></p>
+                            <p className="Tags"> semester : <span className="User Department" >Null</span></p>
                         </div>
-                        <div className="profileCol4" >
-
+                        <div className="profileCol3" >
+                        <p className="Tags">Email: <span className="User Name" >proyash20-331@aec.ac.in</span></p>
+                            <p className="Tags">address: <span className="User rollNo" >Panikara Gaon, Morangi Chari Ali, dist: Golaghat Golaghat, Assam, 785702 India</span></p>
+                            <p className="Tags">phoneNumber: <span className="User course" >6913911634</span></p>
+                            <p className="Tags"> semester : <span className="User Department" >Null</span></p>
                         </div>
                     </div>
                 </div>
