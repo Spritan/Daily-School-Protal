3c50293497df91c6d3ec679a3f87004e345b8dc0
Spritan <proyashpsb@gmail.com>
2022-05-09 17:24:28 +0530

    backup

--- client/student/src/pages/profile/Profile.jsx
+++ client/student/src/pages/profile/Profile.jsx
@@ -9,13 +9,32 @@ const Profile = () => {
             <div className="container">
                 <div className="flex"></div>
                 <div className="profileBody">
-                    <div className="profileCol1">
-                        <img src="" alt="" />
+                    <div className="profileBody1">
+                        <div className="profileCol1">
+                            <img 
+                            className="UserImg"
+                            src="https://th.bing.com/th/id/OIP.p-tCU_opnL7Co30gF4n9EwHaFq?pid=ImgDet&rs=1" 
+                            alt="" />
+                        </div>
+                        <div className="profileCol2">
+                            <h1>Details</h1>
+                            <p className="Tags">Name: <span className="User Name" >Proyash Paban Sarma Borah</span></p>
+                            <p className="Tags">Roll No.: <span className="User rollNo" >123</span></p>
+                            <p className="Tags">Course: <span className="User course" >123</span></p>
+                            <p className="Tags">Depatment: <span className="User Department" >Electronics and Telecommunication Engineering</span></p>
+                        </div>
                     </div>
-                    <div className="profileCol2">
+                    <div className="profileBody2">
+                        <div className="profileCol3">
+                            <p className="Tags">Name: <span className="User Name" >Proyash Paban Sarma Borah</span></p>
+                            <p className="Tags">Roll No.: <span className="User rollNo" >123</span></p>
+                            <p className="Tags">Course: <span className="User course" >123</span></p>
+                            <p className="Tags">Depatment: <span className="User Department" >Electronics and Telecommunication Engineering</span></p>
+                        </div>
+                        <div className="profileCol4" >
 
+                        </div>
                     </div>
-
                 </div>
                 <div className="flex"></div>
             </div>
